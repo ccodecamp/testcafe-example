9570d9daada64786d5e7aa53482f28335c274c23
Chaitanya Kulkarni <kulkarnichaitanyav@gmail.com>
2019-07-12 22:05:05 +1000

    Updating tests

--- page/resultspage.js
+++ page/resultspage.js
@@ -2,5 +2,8 @@ import { Selector } from 'testcafe';
 
 export default class ResultsPage {
     constructor() {
+        this.searchBar = Selector('div[class="SDkEP"]')
+        this.menuBar = Selector('#hdtb-msb-vis')
+        this.signInButton = Selector('#gb_70')
     }
 }
--- tests/searchflow.js
+++ tests/searchflow.js
@@ -1,18 +1,20 @@
 import SearchPage from '../page/searchpage';
 import ResultsPage from '../page/resultspage';
+import config from '../config.json';
+
 
 const searchPage = new SearchPage();
 const resultsPage = new ResultsPage();
 
 const SearchUser = async (t) => {
   await t
-    .typeText(searchPage.searchBox, "")
-    .click(searchPage.searchButton);
+    .typeText(searchPage.searchBox, "cypress.io")
+    .pressKey('enter');
 };
 
 
 fixture("Search Page Tests")
-.page("https://www.google.com.au/");
+.page(`${config.SEARCH_PROVIDER_URL}`)
 
 test("Check Search Page", async (t) => {
   await SearchUser(t);
